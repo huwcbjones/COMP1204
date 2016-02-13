28a422cd44d610efdd3380ce8478a9a0e09496c4
Huw Jones <huwcbjones@gmail.com>
2016-02-13 11:52:05 +0000

    Fixed averagereviews.sh

--- Coursework_1/averagereviews.sh
+++ Coursework_1/averagereviews.sh
@@ -6,25 +6,24 @@ function getReviewCount () {
 }
 
 # Gets average score to 1dp
-#function averageScore() {
-#	t=$1
-#	n=$2
-#	
-#	mean=$((t/n))
-#	dp=$(( 10 * (t % n) / n ))
-#	
-#	echo "$mean.$dp"
-#}
+function averageScore() {
+	t=$1
+	n=$2
+	
+	mean=$((t/n))
+	dp=$(( 10 * (t % n) / n ))
+	
+	echo "$mean.$dp"
+}
 
 # Gets the sum of all review scores
 function getTotalScore() {
-	Scores=$(grep "<Overall>" $1 | sed -e 's:<Overall>::')
-	echo $Scores
-#	TotalScore=0
-#	while read -r line; do
-#		TotalScore=$((TotalScore + line))
-#	done < "$Scores"
-#	echo $TotalScore
+	Scores=$(grep "<Overall>" $1 | sed -e 's:<Overall>::' -e 's:\r::')
+	TotalScore=0
+	while read -r line; do
+		TotalScore=$((TotalScore + line))
+	done <<< "$Scores"
+	echo $TotalScore
 }
 
 # Gets the average review of the hotel
@@ -32,27 +31,28 @@ function getAverageScore() {
 	HotelFile=$1
 
 	ReviewCount=$(getReviewCount $1)
-#	echo "ReviewCount: $ReviewCount"
-#	TotalScore=$(getTotalScore $HotelFile)
-#	echo "TotalScore: $TotalScore"
-#	echo $(averageScore $TotalScore $ReviewCount)
+	TotalScore=$(getTotalScore $HotelFile)
+	echo $(averageScore $TotalScore $ReviewCount)
 }
 
 # Gets the hotel ID from hotel file
 function getTrimmedHotelFile() {
-	echo $1 | sed -e 's:^[a-zA-Z_0-9\-]*\/::' -e 's:.dat::'
+	echo $1 | sed -e 's:^[/a-zA-Z_0-9\-]*\/::' -e 's:.dat::'
 }
 
 # Checks argument passed was a directory
 if [ -d $1 ]
 then
+	hotels=""
 	# Loops through all files and prints HOTEL_ID AVERAGE_REVIEW
 	for file in $1/*
 	do
-#		echo $(getTrimmedHotelFile $1 $file) $(getTotalScore $file)
-		echo $(getTrimmedHotelFile $1 $file) $(getAverageScore $file)
+		currentHotel="$(getTrimmedHotelFile $file)"$'\t'"$(getAverageScore $file)"
+		hotels="$hotels"$'\n'"$currentHotel"
 	done
+
+	# Sort hotels by second column (rating)
+	echo -e "$hotels" | sort -k2nr
 else
-	echo $(getTrimmedHotelFile $1) $(getAverageScore $file)
-#	echo "$1 is not a valid directory."
+	echo "$(getTrimmedHotelFile $1) $(getAverageScore $1)"
 fi
