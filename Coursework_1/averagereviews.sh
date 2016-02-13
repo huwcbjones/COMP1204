3abc26bbaf4a97962c595a95b76fd14c3a9b3c0b
Huw Jones <huwcbjones@gmail.com>
2016-02-13 11:10:42 +0000

    Used sed in script

--- Coursework_1/averagereviews.sh
+++ Coursework_1/averagereviews.sh
@@ -6,42 +6,41 @@ function getReviewCount () {
 }
 
 # Gets average score to 1dp
-function averageScore() {
-	t=$1
-	n=$2
-	
-	mean=$((t/n))
-	dp=$(( 10 * (t % n) / n ))
-	
-	echo "$mean.$dp"
-}
+#function averageScore() {
+#	t=$1
+#	n=$2
+#	
+#	mean=$((t/n))
+#	dp=$(( 10 * (t % n) / n ))
+#	
+#	echo "$mean.$dp"
+#}
 
 # Gets the sum of all review scores
 function getTotalScore() {
-	Scores=$(grep "<Overall>" $1)
-	TotalScore=0
-	while read -r line; do
-		TotalScore=$((TotalScore + ${line:(-1)}))
-	done <<< "$Scores"
-	echo $TotalScore
+	Scores=$(grep "<Overall>" $1 | sed -e 's:<Overall>::')
+	echo $Scores
+#	TotalScore=0
+#	while read -r line; do
+#		TotalScore=$((TotalScore + line))
+#	done < "$Scores"
+#	echo $TotalScore
 }
 
 # Gets the average review of the hotel
 function getAverageScore() {
 	HotelFile=$1
-	
-	ReviewCount=$(getReviewCount $HotelFile)
-	TotalScore=$(getTotalScore $HotelFile)
-	echo $(averageScore $TotalScore $ReviewCount)
+
+	ReviewCount=$(getReviewCount $1)
+#	echo "ReviewCount: $ReviewCount"
+#	TotalScore=$(getTotalScore $HotelFile)
+#	echo "TotalScore: $TotalScore"
+#	echo $(averageScore $TotalScore $ReviewCount)
 }
 
 # Gets the hotel ID from hotel file
 function getTrimmedHotelFile() {
-	filePath=$1
-	fileName=$2
-	hotelName=${fileName#$filePath/}
-	hotelName=${hotelName%.dat}
-	echo $hotelName
+	echo $1 | sed -e 's:^[a-zA-Z_0-9\-]*\/::' -e 's:.dat::'
 }
 
 # Checks argument passed was a directory
@@ -50,8 +49,10 @@ then
 	# Loops through all files and prints HOTEL_ID AVERAGE_REVIEW
 	for file in $1/*
 	do
+#		echo $(getTrimmedHotelFile $1 $file) $(getTotalScore $file)
 		echo $(getTrimmedHotelFile $1 $file) $(getAverageScore $file)
 	done
 else
-	echo "$1 is not a valid directory."
+	echo $(getTrimmedHotelFile $1) $(getAverageScore $file)
+#	echo "$1 is not a valid directory."
 fi
