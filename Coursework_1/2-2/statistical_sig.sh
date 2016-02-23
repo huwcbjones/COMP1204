18aa758202b6b43ca8b70e80448e928ecb39a8d1
Huw Jones <huwcbjones@gmail.com>
2016-02-23 14:08:21 +0000

    Added SD calculation

--- Coursework_1/2-2/statistical_sig.sh
+++ Coursework_1/2-2/statistical_sig.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+function getScores() {
+	grep "<Overall>" $1 | sed -e 's:<Overall>::'
+}
 
 function getAverageScore() {
-	Scores=$(grep "<Overall>" $1 | sed -e 's:<Overall>::')
-	echo $(echo -e "$Scores" | awk '
+	echo $(getScores $1 | awk '
 	BEGIN {
 		TotalScore=0;
 		n=0;
@@ -14,9 +16,29 @@ function getAverageScore() {
 	}
 
 	END {
-		printf ("%.1f\n", (TotalScore / n));
+		printf ("%.2f\n", (TotalScore / n));
 	}
 	')
+}
+function getSD() {
+	file=$1
+	mean=$2
+	echo -e "$(getScores $1)" | awk '
+		BEGIN {
+			Total=0;
+			n=0;
+		}
+
+		{
+			Total += (($0 - $'$mean')^2);
+			n++;
+		}
+
+		END {
+			var = Total/(n-1);
+			printf ("%.2f\t%.2f\t%.2f\n", Total, var, sqrt(var));
+		}
+	'
 }
 
 # Gets the hotel ID from hotel file
@@ -34,8 +56,9 @@ hotel2=$2
 # Convert hotel IDs to file-names/paths
 hotel1_file=$(echo $hotel1.dat)
 hotel2_file=$(echo $hotel2.dat)
-
-echo hotel1_mean=$(getAverageScore $hotel1_file)
+hotel1_mean=$(getAverageScore $hotel1_file)
+echo "hotel1_mean=$hotel1_mean"
+echo hotel1_sd=$(getSD $hotel1_file $hotel1_mean)
 
 # Check the hotel files exist
 if [ ! -f $hotel1_file ]
@@ -54,7 +77,7 @@ hotel1_mean=$(getAverageScore $hotel1_file)
 hotel2_mean=$(getAverageScore $hotel2_file)
 
 # Get t-statistic
-hotel1_sd=""
+hotel1_sd=$(getSD $hotel1_file $hotel1_mean)
 hotel2_sd=""
 
 echo "Mean $hotel1: $hotel_1_mean, SD: $hotel1_sd"
