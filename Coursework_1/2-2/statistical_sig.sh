dde82c1cbdb8b017feadfb93bb41a70df86479bd
Huw Jones <huwcbjones@gmail.com>
2016-02-23 13:21:52 +0000

    Updated slightly

--- Coursework_1/2-2/statistical_sig.sh
+++ Coursework_1/2-2/statistical_sig.sh
@@ -19,10 +19,10 @@ function averageScore() {
 # Gets the sum of all review scores
 function getTotalScore() {
 	Scores=$(grep "<Overall>" $1 | sed -e 's:<Overall>::' -e 's:\r::')
-	TotalScore=0
-	while read -r line; do
-		TotalScore=$((TotalScore + line))
-	done <<< "$Scores"
+	TotalScore=$(awk -e 'BEGIN {$x=0} {x+= $0} END {print $x}')
+	#while read -r line; do
+	#	TotalScore=$((TotalScore + line))
+	#done <<< "$Scores"
 	echo $TotalScore
 }
 
@@ -48,17 +48,30 @@ hotel1=$1
 hotel2=$2
 
 # Convert hotel IDs to file-names/paths
+hotel1_file=$(echo $hotel1.dat)
+hotel2_file=$(echo $hotel2.dat)
 
+echo hotel1_mean=$(getAverageScore $hotel1_file)
 
 # Check the hotel files exist
+if [ ! -f $hotel1_file ]
+then
+	echo "First hotel was not found."
+	exit
+fi
+if [ ! -f $hotel2_file ]
+then
+	echo "Second hotel was not found."
+	exit
+fi
 
 # Get hotel means
-hotel1_mean=$(getAverageScore $hotel1)
-hotel2_mean=$(getAverageScore $hotel2)
+hotel1_mean=$(getAverageScore $hotel1_file)
+hotel2_mean=$(getAverageScore $hotel2_file)
 
 # Get t-statistic
 hotel1_sd=""
 hotel2_sd=""
 
-echo "Mean $hotel_1: $hotel_1_mean, SD: $hotel1_sd"
-echo "Mean $hotel_2: $hotel_2_mean, SD: $hotel2_sd"
+echo "Mean $hotel1: $hotel_1_mean, SD: $hotel1_sd"
+echo "Mean $hotel2: $hotel_2_mean, SD: $hotel2_sd"
